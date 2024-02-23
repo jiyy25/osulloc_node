46f04ad48fb2f90210876b4bff86614fec6595a7
DESKTOP-0GF5LUT\jiyy8 <jiyy825@gmail.com>
2024-02-23 13:09:40 +0900

    router이동시 스크롤 상단에서 시작 적용

--- osulloc/src/index.js
+++ osulloc/src/index.js
@@ -3,12 +3,13 @@ import * as React from 'react'
 import ReactDOM from 'react-dom/client';
 import App from './App.js';
 import { BrowserRouter } from 'react-router-dom';
-
+import ScrollToTop from './component/Scroll.js';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root?.render(
   <BrowserRouter>
+    <ScrollToTop />
     <App />
   </BrowserRouter>
 
